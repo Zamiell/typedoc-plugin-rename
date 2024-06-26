22a25d66ebaffe66fc8d58a2cc4d24b5cab56cd6
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-06-26 18:13:00 -0400

    fix: import typo

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const td = require("typedoc");
+const typedoc = require("typedoc");
 
 const JSDOC_TAG_FOR_RENAME = "@rename";
 
-exports.load = function (app) {
-  app.converter.on(td.Converter.EVENT_CREATE_DECLARATION, onDeclaration);
+exports.load = function (application) {
+  application.converter.on(typedoc.Converter.EVENT_CREATE_DECLARATION, onDeclaration);
 
-  app.on(Application.EVENT_BOOTSTRAP_END, () => {
-    const modifiers = app.options.getValue("modifierTags");
+  application.on(typedoc.Application.EVENT_BOOTSTRAP_END, () => {
+    const modifiers = application.options.getValue("modifierTags");
     if (!modifiers.includes(JSDOC_TAG_FOR_RENAME)) {
-      app.options.setValue("modifierTags", [...modifiers, JSDOC_TAG_FOR_RENAME]);
+      application.options.setValue("modifierTags", [...modifiers, JSDOC_TAG_FOR_RENAME]);
     }
   });
 };
@@ -20,7 +20,7 @@ exports.load = function (app) {
 function onDeclaration(_context, reflection) {
   const rename = reflection.comment?.getTag(JSDOC_TAG_FOR_RENAME);
   if (rename) {
-    reflection.name = td.Comment.combineDisplayParts(rename.content).trim();
+    reflection.name = typedoc.Comment.combineDisplayParts(rename.content).trim();
     reflection.comment.removeTags(JSDOC_TAG_FOR_RENAME);
   }
 }
